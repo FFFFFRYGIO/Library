a4f1a03bd93f64a0559df99650a4017d437f258f
Radosław <FFFFFRYGIO@gmail.com>
2022-02-22 12:11:59 +0100

    Routing in app, new templates

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return render_template("index.html")
+
+
+@app.route('/books_list')
+def books_list():
+    return render_template("books_list.html")
+
+
+@app.route('/add_books')
+def add_books():
+    return render_template("add_books.html")
+
+
+@app.route('/edit_book')
+def edit_book():
+    return render_template("edit_book.html")
 
 
 if __name__ == '__main__':
