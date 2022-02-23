502dd56ab4dd8aff6065e339bc4e16e71acd3a5a
Radosław <FFFFFRYGIO@gmail.com>
2022-02-23 11:36:46 +0100

    building templates, taking inputs from forms

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from manage_books import get_books, add_books as add, edit_book as edit, get_book_by_isbn
 
 app = Flask(__name__)
 
@@ -8,19 +9,57 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/books_list')
+@app.route('/books_list', methods=["POST", "GET"])
 def books_list():
-    return render_template("books_list.html")
+    if request.method == "POST":
+        book_isbn = request.form['ISBN']
+        book_dict = get_book_by_isbn(book_isbn)
+        return redirect(url_for("edit_book", book_isbn=book_isbn, book=book_dict))
+    else:
+        return render_template("books_list.html", books=get_books())
 
 
-@app.route('/add_books')
+@app.route('/add_books', methods=["POST", "GET"])
 def add_books():
-    return render_template("add_books.html")
+    key_words = {
+        'intitle': 'In the title',
+        'inauthor': 'In the authors',
+        'inpublisher': 'In the publisher',
+        'subject': 'In the category list',
+        'isbn': 'Exact ISBN number',
+        'lccn': 'In the Library of Congress Control Number',
+        'oclc': 'In the Online Computer Library Center number',
+    }
+    if request.method == "POST":
+        book_params = {}
+        for key in key_words:
+            if request.form[key]:
+                book_params[key] = request.form[key]
+        count_errors = add(book_params)  # to flash
+        print('------------', count_errors, '------------')
+        return redirect(url_for("books_list"))
+    else:
+        return render_template("add_books.html", key_words=key_words)
 
 
-@app.route('/edit_book')
+@app.route('/edit_book', methods=["POST", "GET"])
 def edit_book():
-    return render_template("edit_book.html")
+    if request.method == "POST":
+        book_isbn = request.args['book_isbn']
+        book_dict = get_book_by_isbn(book_isbn)
+        book_dict.pop('ISBN')
+        book_config = {}
+        for attr in book_dict:
+            if book_dict[attr] != request.form[attr]:
+                book_config[attr] = request.form[attr]
+        edit(book_isbn, book_config)
+        return redirect(url_for("books_list"))
+    else:
+        book_isbn = request.args['book_isbn']
+        book_dict = get_book_by_isbn(book_isbn)
+        print('\n\n', book_dict, '\n\n')
+        book_dict.pop('ISBN')
+        return render_template("edit_book.html", book_isbn=book_isbn, book=book_dict)
 
 
 if __name__ == '__main__':
