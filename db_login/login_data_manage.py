0eeb98b095b707d851b900295ffbcf49bb0482a9
Radosław <FFFFFRYGIO@gmail.com>
2022-02-26 19:19:01 +0100

    if env (for heroku) - with previous commit

--- db_login/login_data_manage.py
+++ db_login/login_data_manage.py
@@ -40,6 +40,16 @@ def import_login_data(file, key_file):
 
 def init_login_info(admin_file="db_login/admin", user_file="db_login/user", key_file="db_login/key.key",
                     user_login=user_login_data, admin_login=admin_login_data):
+    if os.environ.get['heroku']:
+        heroku_login_data = {
+            'user': os.environ.get['user'],
+            'passwd': os.environ.get['passwd'],
+            'host': os.environ.get['host'],
+            'port': os.environ.get['port'],
+            'db': os.environ.get['db'],
+        }
+        user_login = heroku_login_data
+        admin_login = heroku_login_data
     try:
         write_key(key_file)
         export_login_data(user_login, user_file, key_file)
